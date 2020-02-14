90a5e40b272143e19227315d3cac93fc5794058a
Oleg Eterevsky <oleg@eterevsky.com>
2020-02-14 08:20:00 +0100

    Refactor pos

--- pos.lua
+++ pos.lua
@@ -1,21 +1,25 @@
 local tests = require "tests"
 
+-- Packed positions represent any coordinates between (-2048, -2048) and
+-- (2048, 2048) with a step of 1/256.
 local RADIX = 2 ^ 20
-local HALF_RADIX = 2 ^ 19
+local HALF_RADIX = RADIX / 2
 
-local function unpack(pos)
-  if type(pos) == "number" then
-    local x_scaled = pos % RADIX
-    local y_scaled = math.floor(pos / RADIX)
+local pos = {}
+
+function pos.unpack(p)
+  if type(p) == "number" then
+    local x_scaled = p % RADIX
+    local y_scaled = math.floor(p / RADIX)
     return (x_scaled - HALF_RADIX) / 256, (y_scaled - HALF_RADIX) / 256
   else
-    local x = pos.x or pos[1]
-    local y = pos.y or pos[2]
+    local x = p.x or pos[1]
+    local y = p.y or pos[2]
     return x, y
   end
 end
 
-local function pack(pos_or_x, y)
+function pos.pack(pos_or_x, y)
   local x
   if y == nil then
     if type(pos_or_x) == "number" then return pos_or_x end
@@ -25,6 +29,9 @@ local function pack(pos_or_x, y)
     x = pos_or_x
   end
 
+  assert(-2048 < x and x < 2048)
+  assert(-2048 < y and y < 2048)
+
   local x_scaled = math.floor(x * 256 + 0.5) + HALF_RADIX
   local y_scaled = math.floor(y * 256 + 0.5) + HALF_RADIX
 
@@ -33,105 +40,79 @@ local function pack(pos_or_x, y)
   return x_scaled + RADIX * y_scaled
 end
 
-local function norm(pos_or_x, y)
+function pos.norm(pos_or_x, y)
   if y ~= nil then return {x = pos_or_x, y = y} end
   if type(pos_or_x) == "table" and pos_or_x.x ~= nil then return pos_or_x end
-  local ux, uy = unpack(pos_or_x)
+  local ux, uy = pos.unpack(pos_or_x)
   return {x = ux, y = uy}
 end
 
-local function delta(p1, p2)
-  local x1, y1 = unpack(p1)
-  local x2, y2 = unpack(p2)
-  return {x2 - x1, y2 - y1}
-end
-
-local function dist_l2(p1, p2)
+function pos.dist_l2(p1, p2)
   assert(p1 ~= nil)
   assert(p2 ~= nil)
-  local x1, y1 = unpack(p1)
-  local x2, y2 = unpack(p2)
+  local x1, y1 = pos.unpack(p1)
+  local x2, y2 = pos.unpack(p2)
   local dx = x2 - x1
   local dy = y2 - y1
   return math.sqrt(dx * dx + dy * dy)
 end
 
-local function dist_linf(p1, p2)
-  local x1, y1 = unpack(p1)
-  local x2, y2 = unpack(p2)
-  local dx = x2 - x1
-  local dy = y2 - y1
-  return math.max(math.abs(dx), math.abs(dy))
-end
-
 -- Returns a single value that can be added to a packed position so that (x, y)
 -- is transformed to (x + dx, y + dy)
-local function pack_delta(dx, dy)
-  local p = pack(dx, dy)
-  local p0 = pack(0, 0)
+function pos.pack_delta(dx, dy)
+  local p = pos.pack(dx, dy)
+  local p0 = pos.pack(0, 0)
   return p - p0
 end
 
-local function delta_len(d)
-  local x, y = unpack(d + pack(0, 0))
+-- Length of packed delta.
+function pos.delta_len(d)
+  local x, y = pos.unpack(d + pos.pack(0, 0))
   return math.sqrt(x * x + y * y)
 end
 
-local function test()
+tests.register_test("pos.test", function()
   for x = -10, 10, 239 / 256 do
     for y = -10, 10, 239 / 256 do
-      local pos = pack(x, y)
-      local ux, uy = unpack(pos)
+      local p = pos.pack(x, y)
+      local ux, uy = pos.unpack(p)
       assert(x == ux)
       assert(y == uy)
 
-      local upos = pack(pos)
-      assert(upos == pos)
+      local upos = pos.pack(p)
+      assert(upos == p)
 
-      upos = pack({x, y})
-      assert(upos == pos)
+      upos = pos.pack({x, y})
+      assert(upos == p)
     end
   end
 
-  local ux, uy = unpack(pack(0, 0))
+  local ux, uy = pos.unpack(pos.pack(0, 0))
   assert(ux == 0)
   assert(uy == 0)
 
-  ux, uy = unpack({12, 34})
+  ux, uy = pos.unpack({12, 34})
   assert(ux == 12)
   assert(uy == 34)
 
-  local pos = norm({12, 34})
-  assert(pos.x == 12)
-  assert(pos.y == 34)
-end
+  local p = pos.norm({12, 34})
+  assert(p.x == 12)
+  assert(p.y == 34)
+end)
 
-tests.register_test("pos.test", test)
-
-local function test_pack_delta()
+tests.register_test("pos.test_pack_delta", function()
   for _ = 1, 10 do
     local dx = math.random(-10000, 10000) / 256
     local dy = math.random(-10000, 10000) / 256
     local x = math.random(-10000, 10000) / 256
     local y = math.random(-10000, 10000) / 256
 
-    local p = pack(x, y)
-    local d = pack_delta(dx, dy)
-    local xn, yn = unpack(p + d)
+    local p = pos.pack(x, y)
+    local d = pos.pack_delta(dx, dy)
+    local xn, yn = pos.unpack(p + d)
     assert(xn == x + dx)
     assert(yn == y + dy)
   end
-end
+end)
 
-tests.register_test("pos.test_pack_delta", test_pack_delta)
-
-return {
-  pack = pack,
-  unpack = unpack,
-  norm = norm,
-  delta = delta,
-  dist_l2 = dist_l2,
-  dist_linf = dist_linf,
-  pack_delta = pack_delta,
-  delta_len = delta_len,
-}
+return pos
